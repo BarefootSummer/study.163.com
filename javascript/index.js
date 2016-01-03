95129044fee9c431fe27fbcadd0c846e394b0b41
bareFootSummer <625252325@qq.com>
2016-01-03 23:37:11 +0800

    最新完整代码

---
+++ javascript/index.js
@@ -0,0 +1,428 @@
+(function(){
+	//1.检测顶部通知条,实现点击X后关闭
+	util.ready(function(){
+		var cookiesObj = util.getCookies();
+		var tipsElement = document.getElementById("tips");
+		if(cookiesObj && cookiesObj.topclose){
+			util.addClass(tipsElement, "f-dn");
+		}else{
+			var closeTips = document.getElementById("closeTips");
+			util.addEventListener(closeTips, "click", function(event){
+				util.preventDefault(event);
+				var now = new Date();
+				now.setFullYear(now.getFullYear()+1);
+				util.setCookie({
+					name: "topclose",
+					value: "1",
+					expires: now,
+				})
+				util.addClass(tipsElement, "f-dn");
+			})
+		};
+	});
+
+	//2.关于网易产品 、 登陆
+	util.ready(function(){
+		var attentionEl = document.getElementById("attention");
+		var cookiesObj = util.getCookies();
+		if(cookiesObj && cookiesObj.loginSuc){
+			util.removeClass(attentionEl, "z-show");
+			util.addClass(attentionEl, "z-hide");
+		}else{
+			var guanzhudncEl = document.getElementById("guanzhudnc");
+			var loginModal = document.getElementById("loginModal");
+			var closeLoginModal = document.getElementById("closeLoginModal");
+			var loginUserName = document.getElementById("loginUserName");
+			var loginPassword = document.getElementById("loginPassword");
+			var loginSubmit = document.getElementById("loginSubmit");
+			var loginUrl = "http://study.163.com/webDev/login.htm";
+			var guanzhuUrl = "http://study.163.com/webDev/attention.htm";
+			/**
+			 * 检测登陆的表单验证
+			 * @return {[type]} [description]
+			 */
+			var validateLoginForm = function(){
+				var _flag = true;
+				if(loginUserName.value.length <= 0){
+					_flag = false;
+					util.addClass(loginUserName, "error");
+				}else if (loginPassword.value.length <= 0){
+					_flag = false;
+					util.addClass(loginPassword, "error");
+				}
+				return _flag;
+			}
+			util.addEventListener(loginUserName, "input",function(event){
+				util.removeClass(loginUserName, "error");
+			});
+
+			util.addEventListener(loginPassword, "input",function(event){
+				util.removeClass(loginPassword, "error");
+			});
+
+			util.addEventListener(guanzhudncEl, "click", function(event){
+				util.preventDefault(event);
+				util.removeClass(loginModal,"f-dn");
+			});
+			util.addEventListener(closeLoginModal, "click", function(event){
+				util.addClass(loginModal,"f-dn");
+			});
+			util.addEventListener(loginSubmit, "click",function(event){
+				if(validateLoginForm()){
+					util.ajax({
+						'url': loginUrl,
+						'method': "POST",
+						'header': {
+							'Content-Type': 'application/x-www-form-urlencoded',
+						 },
+						'data': {
+						 	'userName': util.md5(loginUserName.value),
+							'password': util.md5(loginPassword.value),
+						 },
+						 success: function(data){
+						 	if(data == '1'){
+						 		//设置Cookie
+						 		var now = new Date();
+						 		now.setFullYear(now.getFullYear()+1);
+						 		util.setCookie({
+						 			name: "loginSuc",
+						 			value: "1",
+						 			expires: now,
+						 		});
+						 		//调用关注的api
+						 		util.ajax({
+						 			url: guanzhuUrl,
+						 			'header': {
+						 				'Content-Type': 'application/x-www-form-urlencoded',
+						 			 },
+						 			success:function(data){
+						 				if(data == '1'){
+						 					util.setCookie({
+									 			name: "followSuc",
+									 			value: "1",
+									 			expires: now,
+									 		});
+									 		//按钮变成已关注
+									 		util.removeClass(attentionEl, "z-show");
+											util.addClass(attentionEl, "z-hide");
+											util.triggerEventListener(closeLoginModal, "click");
+						 				}
+						 			}
+						 		});
+						 	}else if(data == '0'){
+						 		alert('账号或密码错误!');
+						 	}
+						 },
+					});
+				}
+			});
+		}
+	});
+
+	//4.轮播图
+	util.ready(function(){
+		var mSld = util.getElementsByClassName("m-sld")[0];
+		var imglist = mSld.getElementsByTagName("img");
+		var dotlist = util.getElementsByClassName(mSld, "dot")[0].children;
+		var imgAmount = imglist.length;  //图片数量 
+		var index = 0;//当前图片显示的下标
+		var autoInterval = 5000;    //自动播放图片的切换时间
+		var time = 500;     //渐变总时间
+		var interval = 100;  //每次渐变时间 
+		var internlOpacity = 1/(time/interval); //每次渐变的透明度 
+		var timerId ; //定时器的Id
+		var oldIndex ;
+		util.forEach(imglist, function(item, i){
+			if(i == 0){
+				util.addClass(item.parentElement, "top");
+				util.setOpacity(item, 1);
+			}else{
+				util.setOpacity(item, 0);
+			}
+		})
+
+		//切换下面小圆点
+		var switchDot = function(){
+			util.forEach(dotlist, function(item){
+				if(item.className === "z-show"){
+					util.removeClass(item, "z-show");
+					return ;
+				}	
+			});
+			util.addClass(dotlist[index], "z-show");
+		}
+		/**
+		 * 切换图片
+		 * @param  {[type]} oldIndex [上一张图片的index]
+		 * @param  {[type]} tarIndex [下一张图片的index]
+		 * @return {[type]}          [description]
+		 */
+		var switchImg = function(oldIndex,tarIndex){
+			if(oldIndex == tarIndex){
+				return ;
+			}
+			//渐变函数
+			var gradualChange = function(){
+				if (util.getOpacity(imglist[oldIndex]) > 0){
+				    if(imglist[tarIndex].style.opacity === ''){
+				    	util.setOpacity(imglist[tarIndex], 0);
+				    } 
+				    util.setOpacity(imglist[oldIndex], util.getOpacity(imglist[oldIndex]) - internlOpacity);
+				    util.setOpacity(imglist[tarIndex], parseFloat(util.getOpacity(imglist[tarIndex])) + parseFloat(internlOpacity));
+				    setTimeout(gradualChange, interval);
+				}else{
+				    util.setOpacity(imglist[oldIndex], 0);
+				    util.setOpacity(imglist[tarIndex], 1);
+				    util.addClass(imglist[tarIndex].parentElement, "top");
+				    util.removeClass(imglist[oldIndex].parentElement, "top");
+				}
+			} 
+			gradualChange();	
+		}
+
+		var play = function (){
+			 oldIndex = index;
+			index = (index+1) % imgAmount;
+			timerId = setTimeout(function(){
+				switchImg(oldIndex, index);
+				switchDot();
+				play();
+			}, autoInterval)
+		}
+
+		var stop = function(){
+			index = oldIndex;
+			clearTimeout(timerId)
+		}
+		util.addEventListener(mSld,"mouseout",play);
+		util.addEventListener(mSld,"mouseover",stop);
+
+		play();
+
+	});
+	//5.左侧内容区Tab切换
+	util.ready(function(){
+		var courseUrl = "http://study.163.com/webDev/couresByCategory.htm";
+		var courselist = document.getElementById("courselist");
+		var courselistOuter = document.getElementById("courselistOuter");
+		var tabs = document.getElementById("coursebdNav").getElementsByTagName("a");
+
+
+		//创建课程悬浮窗元素
+		var createdetailCourseElement = function(dataObj){
+			var wrapHtml = ("<div class=\"suspend_course\" id='suspend_course-#{id}'>");
+				wrapHtml +=("<div class=\"suspend_course_main f-cb\">");
+				wrapHtml +=("<img class=\"logo\" src=\" #{middlephotourl} \" alt=\"\">");
+				wrapHtml +=("<div class=\"info\">");
+				wrapHtml +=("<h2 class=\"tt\">#{name}</h2>");
+				wrapHtml +=("<p class=\"pnum\"> #{learnercount} 人在学</p>");
+				wrapHtml +=("<p class=\"autor\">发布者：#{provider}</p>");
+				wrapHtml +=("<p class=\"classify\">分类：#{categoryname}</p>");
+				wrapHtml +=("</div>");
+				wrapHtml +=("</div>");
+				wrapHtml +=("<p class=\"suspend_course_introduce\">#{description}</p>");
+				wrapHtml +=("</div>");
+			var tmpEl = document.createElement('div');
+			tmpEl.innerHTML = wrapHtml.format(dataObj);
+
+			return tmpEl.childNodes[0];
+		}
+		//鼠标移入
+		var mouseenterHandler = function(event){
+			var divX = 0;
+			var divY = 0;
+			event = event || window.event;
+			var target = event.target || event.srcElement;  
+			var courseAreaNode = document.getElementById("courseArea"); 
+			divX = this.offsetLeft + this.clientWidth;
+			divY = this.offsetTop; 
+			var parantWidth=this.offsetParent.clientWidth;
+
+			var dataset = util.getElementDataSet(this);
+
+			var detailNode = createdetailCourseElement(dataset); 
+			//左右适应
+			var flagProcessingwidth=true;
+			if(parantWidth >= 980){
+				if(dataset.index % 4 == 0 ){
+					detailNode.style.left =(parantWidth-this.offsetLeft) + "px";
+					flagProcessingwidth=false;
+				}
+				else if(dataset.index % 4 == 3 ){
+					detailNode.style.left =(divX-this.clientWidth-detailNode.width) + "px";
+					flagProcessingwidth=false;
+				}
+			}else if(parantWidth <= 735){
+				if(dataset.index % 3 == 0 ){
+					detailNode.style.left =(divX-this.clientWidth-detailNode.width) + "px";
+					flagProcessingwidth=false;
+				}
+			}
+
+			if(flagProcessingwidth){
+				detailNode.style.left = (divX + 20) + "px";
+			}
+			detailNode.style.top = divY + "px";
+			courselistOuter.appendChild(detailNode);
+		}
+		//鼠标移出
+		var mouseleaveHandler = function(event){
+			var dataset = util.getElementDataSet(this);
+			var detailNode = document.getElementById("suspend_course-"+dataset.id);
+			courselistOuter.removeChild(detailNode);
+		}
+
+		//创建课程
+		var createCourseElement = function(dataObj,index){
+			var courseUl = document.createElement("li");
+			var courseA = document.createElement("a");
+			courseA.setAttribute("href","http://study.163.com/course/introduction/"+dataObj.id+".htm#/courseDetail")
+			courseA.setAttribute("target","view_window")
+
+			courseA.setAttribute("data-id", dataObj.id);
+			courseA.setAttribute("data-name", dataObj.name);
+			courseA.setAttribute("data-provider", dataObj.provider);
+			courseA.setAttribute("data-learnerCount", dataObj.learnerCount);
+			courseA.setAttribute("data-categoryName", dataObj.categoryName);
+			courseA.setAttribute("data-middlePhotoUrl", dataObj.middlePhotoUrl); 
+			courseA.setAttribute("data-description", dataObj.description); 
+			courseA.setAttribute("data-index",index+1);
+			var wrapHtml =  "<img class='logo' src='#{middlePhotoUrl}' alt='#{name}'>";
+				wrapHtml += "<div class='infoarea'>"
+				wrapHtml += "<h1 class='tt'>#{name}</h1>"
+				wrapHtml += "<h2 class='writer'>#{provider}</h2>"
+				wrapHtml += "<p class='pnum'>#{learnerCount}</p>"
+				wrapHtml += "<p class='price'>￥#{price}</p>"								
+				wrapHtml += "</div>";
+			wrapHtml = wrapHtml.format(dataObj);
+			courseA.innerHTML = wrapHtml;
+			util.addEventListener(courseA,"mouseenter",mouseenterHandler);
+			util.addEventListener(courseA,"mouseleave",mouseleaveHandler);
+			courseUl.appendChild(courseA);
+			return courseUl;
+		}
+		//执行ajax查询
+		var go = function(ctype){
+			util.ajax({
+				'url': courseUrl,
+				'header': {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				 },
+				'data': {
+					'pageNo': 1,
+					'psize': 20,
+					'type': ctype,
+				},
+				'success' : function(data){
+					courselist.innerHTML = '';
+					var courseDataObj = JSON.parse(data);
+					var courseList = courseDataObj.list;
+
+					util.forEach(courseList, function(item,i){
+						courselist.appendChild(createCourseElement(item,i));
+					});
+				}
+			})
+		}
+
+		util.forEach(tabs, function(item){
+			util.addEventListener(item, "click", function(event){
+				util.preventDefault(event);
+				var target = event.target || event.srcElement;
+				util.forEach(tabs, function(item){
+					if(item.className === "z-crl"){
+						util.removeClass(item, "z-crl");
+						return ;
+					}	
+				});
+				util.addClass(target, "z-crl");
+				var dataset = util.getElementDataSet(target);
+				var ctype = dataset  && dataset.type;
+				go(ctype || 10);//如果获取不到默认为10
+			})
+		});
+
+		//触发事件
+		util.triggerEventListener(tabs[0], "click");
+	});
+	//7.视频弹框
+	util.ready(function(){
+		var openVideo =document.getElementById("openVideo");
+		var closeVideo = document.getElementById("closeVideo");
+
+		util.addEventListener(openVideo, "click", function(event){
+			util.removeClass(videoModal,"f-dn");
+		})
+		util.addEventListener(closeVideo, "click", function(event){
+			util.addClass(videoModal,"f-dn");
+		})
+	});
+
+	//8.热门推荐
+	util.ready(function(){
+		var hotIntervalId;
+		var hotNode = document.getElementById("rmph");
+
+		//创建课程节点
+		var createHotCrs = function(dataObj){
+			var wrapHtml = ("<li>");
+			wrapHtml += ("<img src=\"#{smallPhotoUrl}\">");
+			wrapHtml += ("<h2>#{name}</h2>");
+			wrapHtml += ("<span>#{learnerCount}</span>");
+			wrapHtml += ("</li>");
+			var tmpEl = document.createElement('div');
+			tmpEl.innerHTML = wrapHtml.format(dataObj);
+			return tmpEl.childNodes[0];
+		}
+		//请求数据
+		util.ajax({
+			url: "http://study.163.com/webDev/hotcouresByCategory.htm",
+			success: function(data){
+				var hotCrsList = JSON.parse(data);
+				util.forEach(hotCrsList, function(item){
+					hotNode.appendChild(createHotCrs(item));
+				});
+			}
+		});
+
+		//每5秒自动更换热点课程
+		var switchHotFn = function(){
+			//每次滚动的高度
+			var _eachHeight=70; 
+			var _top = 0,
+				_timer;
+			_timer = setInterval(function(){
+				_top += 3;
+				hotNode.style.marginTop = "-"+_top + 'px';
+				if(_top > _eachHeight){
+					clearInterval(_timer);
+
+					//上一个结点
+					var _onNode = hotNode.children[0]
+
+					//重复滚动
+					if(_onNode.nodeType == 1){
+						hotNode.removeChild(_onNode);
+						hotNode.appendChild(_onNode);
+						hotNode.style.marginTop = "0px"; 
+					}					
+				}
+			},30);
+
+
+		}
+		//轮播热点课程
+		hotIntervalId = setInterval(switchHotFn, 5000); 
+
+		//绑定mouseover事件
+		util.addEventListener(hotNode,"mouseover",function(){
+			clearInterval(hotIntervalId);
+		});
+
+		//绑定mouseout事件
+		util.addEventListener(hotNode,"mouseout",function(){
+			hotIntervalId = setInterval(switchHotFn, 5000); 
+		});
+	});
+
+})();
---
+++ javascript/util.js
@@ -0,0 +1,471 @@
+/**
+ * 一些常用的方法 
+ * @author ipengyo@qq.com
+ * @param  {[type]} global [description]
+ * @return {[type]}        [description]
+ */
+(function(global){
+	if(!global.util){
+		var util = global.util = {};
+	}
+	/*ie678判定*/
+	var ie678 = util.ie678 = /\w/.test('\u0130');
+	var d = document ;
+
+	//对String 的原型进行扩展,参考自深入浅出Nodejs @author ipengyo@qq.com
+	String.prototype.format = (function(){
+		var regCache = [];
+		var numberReg = /\#{(\d+)\}/g;
+		return function(args) {
+			var funArguments = arguments;
+		    var result = this;
+		    if (arguments.length > 0) {
+		        if (arguments.length == 1 && typeof(args) == "object") {
+		            for (var key in args) {
+		                var reg = regCache[key] || (regCache = new RegExp("(#{" + key + "})", "g"));
+		                result = result.replace(reg, args[key]);
+		            }
+		        } else {
+            	    result = result.replace(numberReg,function(m,x){  
+            	           return funArguments[x];  
+            	     });  
+		        }
+		        return result;
+		    } else {
+		        return this;
+		    }
+		} 
+	})(); 
+
+    /**
+     * 数组遍历函数
+     * @param {Array} array 待遍历的数组
+     * @param {Function} fn 回调函数
+     * @ignore
+     */
+    var forEach = util.forEach = function(array, fn) {
+        for (var i = 0, len = array.length; i < len; i++) {
+            fn(array[i], i);
+        }
+    }
+
+	/**
+	 * 扩展，当一个参数的时候向自己扩展 多个参数的时候，后面的向第一个扩展，并覆盖
+	 * @param  {[type]} obj [description]
+	 * @return {[type]}     [description]
+	 */
+	var extend = util.extend = function(obj){
+		if(obj === void 0){
+			return this;
+		}
+		if(arguments.length == 1){
+			for(var p in obj){
+				if(obj.hasOwnProperty(p)){
+					this[p] = obj[p];
+				}
+			}
+			return this;
+		}
+
+		forEach([].slice.call(arguments,1), function(item){
+			for(var p in item){
+				if(item.hasOwnProperty(p)){
+					obj[p] = item[p];
+				}
+			}
+		});	
+		return obj;
+	}
+
+	
+	/**
+	 * 将对象转为参数化的 
+	 * @author ipengyo@qq.com
+	 * @param  {[type]} obj [description]
+	 * @return {[type]}     [description]
+	 */
+	var serializeObj_str = util.serializeObj_str = function(obj){
+		if (!obj) return "";
+		var arr = [];
+		for(key in obj){
+			if(obj.hasOwnProperty(key) && typeof(obj[key]) != 'function'){
+				var name = encodeURIComponent(key);
+				var value = encodeURIComponent(obj[key].toString());
+				arr[arr.length] = name +"="+ value;
+			}
+		}
+		return arr.join("&");
+	};
+	/**
+	 * 获取当前文档的Cookies,返回Cookie对象
+	 * @return {[type]} [description]
+	 */
+	var getCookies = util.getCookies = function(){
+		var cookieObj = {};
+		var all = d.cookie;
+		if(!all) return cookieObj;
+		var listCookie = all.split("; ");
+		forEach(listCookie, function(item){
+	      var p = item.indexOf('=');
+	      var name = item.substring(0, p);
+	      name = decodeURIComponent(name);
+	      var value = item.substring(p + 1);
+	      value = decodeURIComponent(value);
+	      cookieObj[name] = value;
+		});
+		return cookieObj;
+	}
+	/**
+	 * 设置cookie {name,value,path,domail,secure}
+	 * @param {[type]} opt [description]
+	 */
+	var setCookie = util.setCookie = function(opt){
+	 var cookie = encodeURIComponent(opt.name)+'='+ encodeURIComponent(opt.value);
+	  if (opt.expires)
+	    cookie += '; expires=' + opt.expires.toGMTString();
+	  if (opt.path)
+	    cookie += '; path=' + opt.path;
+	  if (opt.domain)
+	    cookie += '; domain=' + opt.domain;
+	  if (opt.secure)
+	     cookie += '; secure=' + opt.secure;
+	 	 document.cookie = cookie;
+	}
+	/**
+	 * 删除cookie{name path domain}
+	 * @param  {[type]} opt [description]
+	 * @return {[type]}     [description]
+	 */
+	var removeCookie = util.removeCookie = function(opt){
+		 document.cookie = 'name=' + opt.name + '; path=' + opt.path + '; domain=' + opt.domain + '; max-age=0';
+	}
+
+
+	/**
+	 * 生成关于Class的正则
+	 * @param  {Object} ) {                   var cache [description]
+	 * @return {[type]}   [description]
+	 */
+    var classReg = (function () {
+        var cache = {};
+        return function (name) {
+            return cache[name] || (cache[name] = new RegExp('(^|\\s)' + name + '(\\s|$)'));
+        };
+    })();
+
+	/**
+	 * 检测某个节点是否拥有该className
+	 * @param  {[type]}  el        [description]
+	 * @param  {[type]}  className [description]
+	 * @return {Boolean}           [description]
+	 */
+	var hasClass = util.hasClass = function(el , className){
+		if(className && el){
+			return classReg(className).test(el.className);
+		}
+	};
+
+	/**
+	 * 给某个节点添加class
+	 */
+	var addClass = util.addClass = function(el , classNames){
+		if(classNames && el){
+			var oldClassName = el.className;
+			var classArr = [];
+			forEach(classNames.split("/\s+/"),function(item){
+				 !hasClass(el, item) && classArr.push(item);
+			})
+			classArr.length > 0 && (el.className += (oldClassName ? " " : "") + classArr.join(" "));
+		}
+	};
+	/**
+	 * 删除某个节点的className
+	 * @param  {[type]} el         [description]
+	 * @param  {[type]} classNames [description]
+	 * @return {[type]}            [description]
+	 */
+	var removeClass = util.removeClass = function(el, classNames){
+		if(el && classNames){
+			var oldClassName = el.className;
+			forEach(classNames.split("/\s/"),function(item){
+				oldClassName = oldClassName.replace(classReg(item), ' ');
+			})
+			el.className = oldClassName;
+		}
+	};
+	 //第二个参数:是否冒泡,第三个参数:是否可以preventDefault阻止事件
+	var createEvent = function(type, isbubbling, ispreventDefault){
+		var event = document.createEvent('Events');
+         event.initEvent(type, isbubbling || true, ispreventDefault || true);
+         return event;
+	}
+	/**
+	 * 给el新增事件
+	 * @param {[type]} el      [description]
+	 * @param {[type]} type    [description]
+	 * @param {[type]} handler [description]
+	 */
+	var addEventListener = util.addEventListener = function(el, type, handler){
+		if(el.addEventListener){
+			el.addEventListener(type,handler,false);
+		}else if(el.attachEvent){
+			el.attachEvent('on'+type,handler);
+		}else{
+			el['on'+type]=handler;
+		}
+	};
+	/**
+	 * 给el删除事件
+	 * @param  {[type]} el      [description]
+	 * @param  {[type]} type    [description]
+	 * @param  {[type]} handler [description]
+	 * @return {[type]}         [description]
+	 */
+	var removeEventListener = util.removeEventListener = function(el, type, handler){
+		if(element.removeEventListener){
+			element.removeEventListener(type,handler,false);
+		}else if(element.detachEvent){
+			element.detachEvent('on'+type,handler);
+		}else{
+			element['on'+type]=null;
+		}
+	};
+	/**
+	 * 给el触发事件
+	 * @param  {[type]} el   [description]
+	 * @param  {[type]} type [description]
+	 * @return {[type]}      [description]
+	 */
+	var triggerEventListener = util.triggerEventListener = function(el, type){
+		if(el && type){
+			if(el.dispatchEvent){
+				el.dispatchEvent(createEvent(type));
+			//IE
+			}else if(el.fireEvent){
+				el.fireEvent(createEvent(type));
+			}
+		}
+	}
+
+	/**
+	 * 传入事件对象 组织该事件的默认事件
+	 * @param  {[type]} event [description]
+	 * @return {[type]}       [description]
+	 */
+	var preventDefault = util.preventDefault = function(event){
+		if(event.preventDefault){
+			event.preventDefault();
+		}else{
+			event.returnValue = false;
+		}
+	};
+
+	/**
+	 * 根据Class获取元素
+	 * @param  {[type]} ){	               if(d.getElementsByClassName){	    return function(oEl, sClass, sEle){	    	oEl [description]
+	 * @return {[type]}      [description]
+	 */
+	var getElementsByClassName = util.getElementsByClassName =(function(){
+	  if(d.getElementsByClassName){
+	    return function(oEl, sClass){
+	    	if(arguments.length == 0){
+	    		return ;
+	    	}
+	    	if(arguments.length == 1){
+	    		sClass = oEl;
+	    		oEl =  d;
+	    	}
+	    	return oEl.getElementsByClassName(sClass);
+	    }
+	  }else{
+	  	 return function(oEl, sClass, sEle){
+	  	 	oEl = oEl || d;
+		   	var aEle=oEl.getElementsByTagName(sEle || '*'),
+		        arr = [];
+		     forEach (aEle, function(){
+		     	classReg(sClass).test(sClass) && arr.push(item);
+		     });
+		    return arr;
+		  }
+	  } 
+	})();
+	/**
+	 * 给元素设置透明度
+	 * @param  {[type]} el    [description]
+	 * @param  {[type]} value [description]
+	 * @return {[type]}       [description]
+	 */
+	var setOpacity = util.setOpacity = function(el, value){
+		if(ie678){
+	      // element.style.filter = "Alpha(opacity=" + value*100 + ")";
+	      el.style.filter = "progid:DXImageTransform.Microsoft.Alpha(opacity=" + value*100 + ")";
+	    }else{
+	      el.style.opacity = value;
+	    }
+	}
+	/**
+	 * 获取元素透明度
+	 * @param  {[type]} element [description]
+	 * @return {[type]}         [description]
+	 */
+	var getOpacity = util.getOpacity = function(el){
+	    if(ie678){
+	      var filterAttr = el.style.filter;
+	      if(filterAttr === ""){
+	        return 0;
+	      }
+	      var opacity = filterAttr.substring(filterAttr.indexOf("=") + 1, filterAttr.lastIndexOf(")"));
+	      return parseFloat(opacity) / 100;
+	    }else{
+	      return el.style.opacity;
+	    }
+	}
+	var datasetReg = /^data-/;
+	var getElementDataSet = util.getElementDataSet = function(el){
+		if(el.dataset) return el.dataset;
+		var oDataset = {};
+		forEach(el.attributes, function(item){
+			var name = element.attributes[i].nodeName;
+   			if(datasetReg.test(name)){
+				oDataset[name] = element.attributes[i].value;
+		   }
+		})
+		return oDataset;
+	};
+
+	/**
+	 * domReady 实现 参考自  baidu  + 自己理解
+	 * @author ipengyo@qq.com
+	 */
+	var ready = util.ready = (function(){
+		var readyList = [];
+		var readyBound = false;
+		var domReadyFn;
+		if(!ie678){
+			domReadyFn = function(){
+				d.removeEventListener("DOMContentLoaded", domReadyFn , false);
+				readyCallBack();
+			}
+		}else{
+			domReadyFn = function(){
+				if(d.readyState = 'complete'){
+					d.detachEvent("onreadystatechange", domReadyFn);
+					readyCallBack();
+				}
+			}
+		}
+		//利用页面未加载完成时 调用documentElement.doScroll方法会爆出异常来实现domReady
+		var doSrcollCheck = function(){
+			try{
+				d.documentElement.doScroll("left");
+			}catch(e){
+				setTimeout(doSrcollCheck, 1);
+				return ;
+			}
+			readyCallBack();
+		}
+
+		var bingReady = function(){
+			if(readyBound){
+				return ;
+			}
+			readyBound = true;
+			if(ie678){
+				d.attachEvent("onreadystatechange", domReadyFn);
+				try{
+					var isTopElement = window.frameElement == null || false ;
+					(isTopElement && d.documentElement.doSrcoll) && doSrcollCheck();
+				}catch(e){}
+			}else{
+				d.addEventListener("DOMContentLoaded", domReadyFn , false );
+			}
+			addEventListener(window, "load" ,domReadyFn);
+		}
+		var readyCallBack = function(){
+			if(!ready.isReady){
+				ready.isReady = true;
+				//执行队列
+				forEach(readyList, function(item){
+					item();
+				})
+			}
+		}
+		bingReady();
+		return function(callback){
+			//没有加载完就直接执行，没有加载完就加到队列里面去
+			ready.isReady ? callback() : readyList.push(callback);
+		}	
+	})();
+	/**
+	 * ajax操作
+	 * @param  {Object} ){		var defaults      [description]
+	 * @return {[type]}          [description]
+	 */
+	var ajax = util.ajax = (function(){
+		var defaults = {
+                method: 'GET',
+                async : true
+        };
+        var xmlHttpReq ;
+        var getXMLHttpReq = function(){
+        	var _xmlHttpReq;
+        	if(window.XMLHttpRequest){
+        		_xmlHttpReq = window.XMLHttpRequest;
+        	}else if(window.ActiveXObject){
+        		//为了兼容IE7以下 浏览器
+        		var versions = [ "MSXML2.XMLHttp.5.0",  "MSXML2.XMLHttp.4.0","MSXML2.XMLHttp.3.0", "MSXML2.XMLHttp","Microsoft.XMLHttp"]; 
+        		forEach(versions, function(item){
+        			try{
+        				_xmlHttpReq = new window.ActiveXObject(item);
+        			}
+        			catch(e){
+        				console.error("IE下ajax对象创建失败");
+        			}
+        		});
+        	}
+        	return _xmlHttpReq;
+        }
+
+        return function(opts){
+        	opts.method && (opts.method = opts.method.toUpperCase());
+        	//浅拷贝
+        	opts = extend({}, defaults, opts);
+        	xmlHttpReq || (xmlHttpReq = getXMLHttpReq())
+        	var xhr = new xmlHttpReq();
+
+     		if(opts.method === "GET" && opts.data != null && opts.data != "undefined"){
+		       opts.url = opts.url + "?" + serializeObj_str(opts.data);
+		    }
+
+        	//打开链接
+        	xhr.open(opts.method, opts.url, opts.async);
+        	//设置header
+            if (opts.header) {
+               for (var p in opts.header) {
+              	 xhr.setRequestHeader(p, opts.header[p]);
+               }
+            }
+        	//监控readyState 改变事件
+        	xhr.onreadystatechange = function(){
+        		if (xhr.readyState==4 ){
+        			var _responseText = xhr.responseText;
+        			if(xhr.status == 200){
+        				var _successFn = opts.success;
+        				typeof(_successFn) == 'function' && _successFn(_responseText);
+        			}else{
+        				var _errorFn = opts.error;
+    					typeof(_errorFn) == 'function' && _errorFn(_responseText);
+        			}
+        		}
+        	}
+
+        	//发送数据
+        	if(opts.method.toUpperCase() === "GET"){
+        	    xhr.send();
+        	}else if(opts.method.toUpperCase() === "POST"){
+        		xhr.send(serializeObj_str(opts.data));
+        	}
+        };
+	})();
+
+})(this);
